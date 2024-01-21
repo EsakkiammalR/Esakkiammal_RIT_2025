6e62227acb4fddd8c8d3463ae650db00a4217943
Esakkiammal.R <137694187+EsakkiammalR@users.noreply.github.com>
2024-01-21 08:11:18 -0800

    Update 21-1-24_if_else.py

--- 21-1-24_if_else.py
+++ 21-1-24_if_else.py
@@ -1,26 +1,26 @@
 #Task
-Given an integer, , perform the following conditional actions:
+#Given an integer, , perform the #following conditional actions:
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
-Input Format
+#If  is odd, print Weird
+#If  is even and in the inclusive range #of  to , print Not Weird
+#If  is even and in the inclusive range #of  to , print Weird
+#If  is even and greater than , print Not #Weird
+#Input Format
 
-A single line containing a positive integer, .
+#A single line containing a positive #integer, .
 
-Constraints
+#Constraints
 
-Output Format
+#Output Format
 
-Print Weird if the number is weird. Otherwise, print Not Weird.
+#Print Weird if the number is weird. #Otherwise, print Not Weird.
 
-Sample Input 0
+#Sample Input 0
 
-3
-Sample Output 0
+#3
+#Sample Output 0
 
-Weird#
+#Weird
 
 #!/bin/python3
 
